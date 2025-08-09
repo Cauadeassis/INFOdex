cc8899d921461a01a1ff81715df7e92f5a6356c1
Cauã <cauadeassis2009@gmail.com>
2025-08-09 19:00:35 +0000

    mudanças do prettier

--- pages/main.js
+++ pages/main.js
@@ -1,28 +1,28 @@
 import { infoClassmates } from "./classmates.js";
-const main = document.querySelector("main")
+const main = document.querySelector("main");
 function createClassmatePhoto(classmate, classmateDiv) {
-    const classmatePhoto = document.createElement("img");
-    classmatePhoto.classList.add("classmatePhoto");
-    classmatePhoto.src = classmate.photo;
-    classmatePhoto.alt = `Foto de ${classmate.name}`;
-    classmateDiv.appendChild(classmatePhoto);
+  const classmatePhoto = document.createElement("img");
+  classmatePhoto.classList.add("classmatePhoto");
+  classmatePhoto.src = classmate.photo;
+  classmatePhoto.alt = `Foto de ${classmate.name}`;
+  classmateDiv.appendChild(classmatePhoto);
 }
 function createClassmateName(classmate, classmateDiv) {
-    const classmateName = document.createElement("p");
-    classmateName.classList.add("classmateName");
-    classmateName.textContent = classmate.name.split(" ")[0];
-    classmateDiv.appendChild(classmateName);
+  const classmateName = document.createElement("p");
+  classmateName.classList.add("classmateName");
+  classmateName.textContent = classmate.name.split(" ")[0];
+  classmateDiv.appendChild(classmateName);
 }
 const fragment = document.createDocumentFragment();
-infoClassmates.forEach(classmate => {
-    const classmateDiv = document.createElement("div");
-    classmateDiv.classList.add("classmateDiv");
-    createClassmatePhoto(classmate, classmateDiv);
-    createClassmateName(classmate, classmateDiv);
-    fragment.appendChild(classmateDiv);
-    classmateDiv.addEventListener("click", () => {
-        localStorage.setItem("classmate", JSON.stringify(classmate));
-        window.location.href = "secondary.html";
-    })
+infoClassmates.forEach((classmate) => {
+  const classmateDiv = document.createElement("div");
+  classmateDiv.classList.add("classmateDiv");
+  createClassmatePhoto(classmate, classmateDiv);
+  createClassmateName(classmate, classmateDiv);
+  fragment.appendChild(classmateDiv);
+  classmateDiv.addEventListener("click", () => {
+    localStorage.setItem("classmate", JSON.stringify(classmate));
+    window.location.href = "secondary.html";
+  });
 });
-main.appendChild(fragment)
+main.appendChild(fragment);
--- pages/secondary.js
+++ pages/secondary.js
@@ -1,15 +1,15 @@
-let classmate = localStorage.getItem("classmate")
+let classmate = localStorage.getItem("classmate");
 classmate = JSON.parse(classmate);
-document.title = (classmate.name.split(" ")[0])
+document.title = classmate.name.split(" ")[0];
 if (classmate.nickname) {
-    const classmateNickname = document.createElement("p");
-    classmateNickname.classList.add("classmateInformation");
-    classmateNickname.textContent = `Apelido: ${classmate.nickname}`;
-    const nicknameContainer = document.getElementById("nicknameContainer")
-    nicknameContainer.appendChild(classmateNickname)
+  const classmateNickname = document.createElement("p");
+  classmateNickname.classList.add("classmateInformation");
+  classmateNickname.textContent = `Apelido: ${classmate.nickname}`;
+  const nicknameContainer = document.getElementById("nicknameContainer");
+  nicknameContainer.appendChild(classmateNickname);
 }
 const classmateDescription = document.createElement("p");
-classmateDescription.classList.add("classmateDescription")
+classmateDescription.classList.add("classmateDescription");
 classmateDescription.textContent = classmate.description;
 const classmatePhoto = document.createElement("img");
 classmatePhoto.classList.add("classmatePhoto");
@@ -20,7 +20,7 @@ photoContainer.appendChild(classmatePhoto);
 const classmateName = document.createElement("p");
 classmateName.classList.add("classmateInformation");
 classmateName.textContent = `Nome: ${classmate.name}`;
-const nameContainer = document.getElementById("nameContainer")
-nameContainer.appendChild(classmateName)
-const bodyDiv = document.getElementById("bodyDiv")
-bodyDiv.appendChild(classmateDescription)
+const nameContainer = document.getElementById("nameContainer");
+nameContainer.appendChild(classmateName);
+const bodyDiv = document.getElementById("bodyDiv");
+bodyDiv.appendChild(classmateDescription);
