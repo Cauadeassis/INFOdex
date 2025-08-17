960ecc3e30f46987c693d9d57314a5b135e5af3e
Cauã <cauadeassis2009@gmail.com>
2025-08-17 18:45:21 +0000

    configurando

---
+++ infra/database.js
@@ -0,0 +1,16 @@
+import { Client } from 'pg';
+async function query(queryObject) {
+  const client = new Client({
+    host: 'localhost',
+    port: '5432',
+    user: 'postgres',
+    database: 'postgres',
+    password: 'mypassword'
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default query;
