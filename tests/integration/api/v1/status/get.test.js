e3ddd64c6e068749815126970a5ab4ff1ab619aa
Cauã <cauadeassis2009@gmail.com>
2025-09-10 09:08:01 +0000

    fiz o teste dar certo

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,7 @@
 test("GET TO /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-})
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(new Date(responseBody.updated_at).toString()).not.toBe("Invalid Date");
+});
