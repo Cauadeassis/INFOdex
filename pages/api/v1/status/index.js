bed5988c1ed554b861f69ffc49b00604ba4da45d
Cauã <cauadeassis2009@gmail.com>
2025-09-13 17:31:35 +0000

    testes automatizados e funções

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js"
-
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database("SHOW server_version;")
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database("SHOW max_connections;")
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+  console.log(databaseOpenedConnectionsValue)
   response.status(200).json({
-    updated_at: updatedAt
-  })
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: parseInt(databaseOpenedConnectionsValue)
+      }
+    }
+  });
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,6 +2,8 @@ test("GET TO /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
   expect(new Date(responseBody.updated_at).toString()).not.toBe("Invalid Date");
+  expect(responseBody.dependencies.database.version).toEqual('16.10')
+  expect(responseBody.dependencies.database.max_connections).toEqual(100)
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1)
 });
